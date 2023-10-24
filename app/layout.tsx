76f1ca1c494fed265716805d1656aad892ebb42d
Toni Aho <toni@tahoo.fi>
2023-10-24 15:30:22 +0300

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Keikat etusivu',
-  description: 'Keikat - Vapaita keikkoja Freelancereille',
-}
+  title: "Keikat etusivu",
+  description: "Keikat - Vapaita keikkoja Freelancereille",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="bg-gray border-gray-200">
+          <div className="flex flex-wrap items-center justify-between p-4 mx-auto">
+
+            <Link href="/" className="flex items-center">Keikat</Link>
+
+          </div>
+        
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
