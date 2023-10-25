e08bcc6d81c98c34cef32a583acd200110e0f63d
Toni Aho <toni@tahoo.fi>
2023-10-25 21:52:22 +0300

    uusi cron muutos

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req, res) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).end("Unauthorized");
+  }
   console.log("MENTIIN NYT TÄNNE AINAKIN!");
   return NextResponse.json({ ok: true });
 }
